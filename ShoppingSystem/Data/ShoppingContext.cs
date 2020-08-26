0f55ebe461a5a63c06bdfac789891f722e00315b
Uliana Tomyn <ulyatomyn@gmail.com>
2020-08-26 22:22:28 +0300

    completed task2

---
+++ ShoppingSystem/Data/ShoppingContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem.Data
+{
+    public class ShoppingContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<SuperMarket> SuperMarkets { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
